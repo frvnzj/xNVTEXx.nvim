7ff649c196c31bebb9f8ed63516a43bfa7f0c362
frvnzj <frvnzj.gmz@gmail.com>
2026-03-26 10:11:47 -0600

    feat: nvim_echo
    Removí fidget y vim.notify, en su lugar preferí el uso exclusivo de
    nvim_echo(), noice puede mostrar el progreso como si fuera un lsp.

--- lua/xJUSTEXx/xJUSTx.lua
+++ lua/xJUSTEXx/xJUSTx.lua
@@ -18,69 +18,62 @@ end
 --- Function to execute a "just" command with optional progress reporting
 ---@param command string: The "just" command to execute
 function M.xCOMPILEx(command)
-  local use_fidget = false
-  local fidget
-
-  if pcall(function()
-    fidget = require("fidget")
-  end) then
-    use_fidget = true
-  end
-
   local cmd = "just " .. command
   local file_name = get_main_file_name() or vim.fn.expand("%:t")
-  local handle
 
-  if use_fidget then
-    handle = fidget.progress.handle.create({
-      title = "Just: " .. command,
-      message = "Compiling " .. file_name .. "...",
-      lsp_client = { name = "xJUSTEXx" },
-      percentage = 0,
-    })
-  else
-    vim.notify("Starting compilation: " .. command, vim.log.levels.INFO)
-  end
+  local message_id = "xJUSTEXx"
 
+  vim.api.nvim_echo({ { "Compiling " .. file_name .. "...", "none" } }, false, {
+    id = message_id,
+    kind = "progress",
+    title = "Just: " .. command,
+    status = "running",
+    percent = 0,
+  })
+
+  --TODO: Tal vez parsear la salida de latexmk
   vim.fn.jobstart(cmd, {
     stdout_buffered = true,
+
     on_stdout = function(_, data)
       if data and #data > 0 then
         local message = "Compiling " .. file_name .. "..."
-        if use_fidget then
-          handle:report({ message = message })
-        else
-          vim.notify(message, vim.log.levels.INFO)
-        end
+
+        vim.api.nvim_echo({ { message .. "...", "None" } }, false, {
+          id = message_id,
+          kind = "progress",
+          status = "running",
+          percent = 60,
+        })
       end
     end,
+
     on_stderr = function(_, data)
       if data and #data > 0 then
-        local message = "Compiling " .. file_name .. "..."
-        if use_fidget then
-          handle:report({ message = message, percentage = 50 })
-        else
-          vim.notify(message, vim.log.levels.WARN)
-        end
+        vim.api.nvim_echo({ { "Processing with warnings...", "WarningMsg" } }, false, {
+          id = message_id,
+          kind = "progress",
+          status = "running",
+          percent = 50,
+        })
       end
     end,
+
     on_exit = function(_, code)
       if code == 0 then
-        local message = "Compilation finished successfully!"
-        if use_fidget then
-          handle:report({ message = message })
-          handle:finish()
-        else
-          vim.notify(message, vim.log.levels.INFO)
-        end
+        vim.api.nvim_echo({ { "Compilation finished successfully!", "MoreMsg" } }, true, {
+          id = message_id,
+          kind = "progress",
+          status = "success",
+          percent = 100,
+        })
       else
-        local message = "Compilation failed! Exit code: " .. code
-        if use_fidget then
-          handle:report({ message = message })
-          handle:finish()
-        else
-          vim.notify(message, vim.log.levels.ERROR)
-        end
+        vim.api.nvim_echo({ { "Compilation failed! Exit code: " .. code, "ErrorMsg" } }, true, {
+          id = message_id,
+          kind = "progress",
+          status = "failed",
+          percent = 100,
+        })
       end
     end,
   })

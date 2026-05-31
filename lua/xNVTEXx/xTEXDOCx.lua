f7d4795eb0ebde2caba3a499a32c2c83073585ee
frvnzj <frvnzj.gmz@gmail.com>
2026-05-30 21:57:28 -0600

    feat: utils
    add utils.notify to xTEXDOCx

--- lua/xNVTEXx/xTEXDOCx.lua
+++ lua/xNVTEXx/xTEXDOCx.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local u = require("xNVTEXx.utils")
+
 --- Function to open LaTeX documentation for the word under the cursor
 function M.xTEXDOCx()
   local package = vim.fn.expand("<cword>")
@@ -7,12 +9,12 @@ function M.xTEXDOCx()
     return
   end
 
-  vim.notify("Looking doc for " .. package .. "...", vim.log.levels.INFO)
+  u.notify("Looking doc for " .. package .. "...")
 
   vim.system({ "texdoc", package }, {}, function(obj)
     if obj.code ~= 0 then
       vim.schedule(function()
-        vim.notify("No doc found for '" .. package .. "'", vim.log.levels.WARN)
+        u.notify_warn("No doc found for '" .. package .. "'")
       end)
     end
   end)
